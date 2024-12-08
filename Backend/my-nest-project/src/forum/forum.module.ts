99f904785fb5eae08707bce0545304c69d9c659a
MayarMohammedd <mayar.mohammed471@gmail.com>
2024-12-08 23:03:46 +0200

    done1

---
+++ Backend/my-nest-project/src/forum/forum.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ForumService } from './forum.services';
+import { ForumController } from './forum.controller';
+import { Forum, ForumSchema } from './models/forums.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Forum.name, schema: ForumSchema }]),
+  ],
+  controllers: [ForumController],
+  providers: [ForumService],
+})
+export class ForumModule {}
