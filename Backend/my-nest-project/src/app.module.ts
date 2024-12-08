57d9401a2ab848d974bd04acb0678dcc7eac28a9
Nancy <nancyahmedmoh@gmail.com>
2024-12-08 23:40:08 +0200

    done

--- Backend/my-nest-project/src/app.module.ts
+++ Backend/my-nest-project/src/app.module.ts
@@ -17,10 +17,14 @@ import { NotificationsModule } from './notifications/notifications.module';
 import { ResourcesModule } from './resources/resources.module';
 import { StudentModule } from './student/student.module';
 import { UserModule } from './user/user.module';
-import { ThreadModule } from './Threads/thread.module';
+import { ThreadModule } from './thread/threads.module';
 import { MessageModule } from './messages/message.module';
-import { ForumModule } from './Forums/forum.module';
-import { ChatModule } from './Chats/chat.module';
+import { ForumModule } from './forum/forum.module';
+import { ChatModule } from './chat/chat.modules';
+import { ThreadMessage } from './threadMessages/models/threadMessages.schema';
+import { ChatMessage } from './chatMessages/models/chatMessages.schema';
+import { ThreadMessageModule } from './threadMessages/threadMessages.module';
+import { ChatMessageModule } from './chatMessages/chatMessages.module';
 
 @Module({
   imports: [
@@ -51,10 +55,15 @@ import { ChatModule } from './Chats/chat.module';
     ResourcesModule,
     StudentModule,
     UserModule,
+    //ThreadModule,
+    //MessageModule,
+    //ForumModule,
+    // ChatModule
     ThreadModule,
-    MessageModule,
-    ForumModule,
-    ChatModule
+    ThreadMessageModule,
+    ChatModule,
+    ChatMessageModule,
+    ForumModule
   ],
   controllers: [AppController],
   providers: [AppService],
