8c8b0e9023d5036378a338e760cb4c94d7b25674
Cisco <manoshalaa@gmail.com>
2025-07-24 00:00:07 +0300

    Fix folder structure

--- Backend/src/thread/threads.module.ts
+++ Backend/src/thread/threads.module.ts
@@ -2,7 +2,7 @@ import { Module ,forwardRef} from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ThreadController } from './threads.controller';
 import { ThreadService } from './threads.services';
-import { Thread, ThreadSchema } from '../thread/models/threads.schema'
+import { Thread, ThreadSchema } from './models/threads.schema'
 
 import { ForumModule } from 'src/forum/forum.module';
 import { Forum, ForumSchema } from 'src/forum/models/forums.schema';
